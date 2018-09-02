e804b3d318ebb94f00b2e7a0e1501bbce6bfbfe4
Viktor Zhurbin <vzhurbin@gmail.com>
2018-09-02 22:31:06 +0300

    add no results message

--- src/components/FlightGrid.js
+++ src/components/FlightGrid.js
@@ -8,72 +8,74 @@ function FlightGrid({ flights, type }) {
   return (
     <div className="flight-table">
       <div className="flight-table__body">
-        {flights.map((flight) => {
-          const {
-            arrivalCity,
-            departureCity,
-            carrierCode,
-            flightNumber,
-            arrivalTime,
-            departureTime,
-            arrivalTerminal,
-            departureTerminal,
-          } = flight;
+        {flights.length < 1
+          ? <div className="no-results">Ничего не найдено</div>
+          : flights.map((flight) => {
+            const {
+              arrivalCity,
+              departureCity,
+              carrierCode,
+              flightNumber,
+              arrivalTime,
+              departureTime,
+              arrivalTerminal,
+              departureTerminal,
+            } = flight;
 
-          return (
-            <div key={`${carrierCode}${flightNumber}`} className="flight-row">
-              <div className="flight-row__block1">
-                <div
-                  className={
-                    `flight-row__time-new
+            return (
+              <div key={`${carrierCode}${flightNumber}`} className="flight-row">
+                <div className="flight-row__block1">
+                  <div
+                    className={
+                      `flight-row__time-new
               ${
-                    type === ARRIVAL
-                      ? `${arrivalTime.old ? 'changed' : ''}`
-                      : `${departureTime.old ? 'changed' : ''}`
-                    }`
-                  }
-                >
-                  {
-                    type === ARRIVAL
-                      ? arrivalTime.new
-                      : departureTime.new
-                  }
-                  <div className="flight-row__time-old">
+                      type === ARRIVAL
+                        ? `${arrivalTime.old ? 'changed' : ''}`
+                        : `${departureTime.old ? 'changed' : ''}`
+                      }`
+                    }
+                  >
                     {
                       type === ARRIVAL
-                        ? arrivalTime.old
-                        : departureTime.old
+                        ? arrivalTime.new
+                        : departureTime.new
+                    }
+                    <div className="flight-row__time-old">
+                      {
+                        type === ARRIVAL
+                          ? arrivalTime.old
+                          : departureTime.old
+                      }
+                    </div>
+                  </div>
+                  <div className="flight-row__city">
+                    {
+                      type === ARRIVAL
+                        ? departureCity
+                        : arrivalCity
                     }
                   </div>
                 </div>
-                <div className="flight-row__city">
-                  {
-                    type === ARRIVAL
-                      ? departureCity
-                      : arrivalCity
-                  }
-                </div>
-              </div>
-              <div className="flight-row__block2">
-                <div className="flight-row__airline">
-                  <span className="flight-row__airline-name">
-                    {carrierCode}
-                  </span>
-                  <span className="flight-row__airline-flight-number">
-                    {flightNumber}
-                  </span>
-                  <div className="flight-row__terminal">
-                    {
-                      type === ARRIVAL
-                        ? (arrivalTerminal || '-')
-                        : (departureTerminal || '-')
-                    }
+                <div className="flight-row__block2">
+                  <div className="flight-row__airline">
+                    <span className="flight-row__airline-name">
+                      {carrierCode}
+                    </span>
+                    <span className="flight-row__airline-flight-number">
+                      {flightNumber}
+                    </span>
+                    <div className="flight-row__terminal">
+                      {
+                        type === ARRIVAL
+                          ? (arrivalTerminal || '-')
+                          : (departureTerminal || '-')
+                      }
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
